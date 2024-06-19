635380c16bab0bf37789422d23fa3cd3596e558a
Vadim1551 <99361409+Vadim1551@users.noreply.github.com>
2024-06-19 13:34:53 +0300

    Update reaction.py

--- reaction.py
+++ reaction.py
@@ -21,8 +21,9 @@ class Reaction:
             exit()
 
     @staticmethod
+    # Находится ли правило в списке правил
     def rule_not_in_table(rule, rules):
-        if rule in rules:
+        if rule in rules:   # O(N)
             return False
         else:
             return True
@@ -34,18 +35,21 @@ class Reaction:
         correct_packet = ARP(op=2, psrc=ip, hwsrc=ip_mac_table[ip])
         send(correct_packet, verbose=0)
 
+    # Блокировка интерфейса
     def block_ip(self, ip_address):
-        commands = []
-        block_incoming = f"sudo iptables -A INPUT -s {ip_address} -j DROP"
-        block_outgoing = f"sudo iptables -A OUTPUT -d {ip_address} -j DROP"
+        commands = []   # O(1)
+        # Создание правил прокировки для iptables
+        block_incoming = f"sudo iptables -A INPUT -s {ip_address} -j DROP"    # O(1)
+        block_outgoing = f"sudo iptables -A OUTPUT -d {ip_address} -j DROP"   # O(1)
 
-        if self.rule_not_in_table(block_incoming[14:], self.rules[1]):
-            commands.append(block_incoming)
-        if self.rule_not_in_table(block_outgoing[14:], self.rules[1]):
-            commands.append(block_outgoing)
-        os.system(" && ".join(commands))
-        self.rules[1] += f"\n{block_incoming[14:]}"
-        self.rules[1] += f"\n{block_outgoing[14:]}"
+        # Проверка на дубликацию правила
+        if self.rule_not_in_table(block_incoming[14:], self.rules[1]):   # O(N)
+            commands.append(block_incoming)   # O(1)
+        if self.rule_not_in_table(block_outgoing[14:], self.rules[1]):   # O(N)
+            commands.append(block_outgoing)   # O(1)
+        os.system(" && ".join(commands))   # O(1)
+        self.rules[1] += f"\n{block_incoming[14:]}"   # O(1)
+        self.rules[1] += f"\n{block_outgoing[14:]}"   # O(1)
 
     def block_mac(self, src_mac):
         commands = []
